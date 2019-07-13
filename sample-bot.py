dbba8d252d7e5d1fa3c9507067d666df6836a075
jared <4jlevy89@gmail.com>
2019-07-13 12:53:42 -0700

    Initial Commit

--- sample-bot.py
+++ sample-bot.py
@@ -10,10 +10,11 @@ from __future__ import print_function
 import sys
 import socket
 import json
+import random
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 # replace REPLACEME with your team name!
-team_name="REPLACEME"
+team_name="BANANAS"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
 test_mode = True
@@ -41,11 +42,58 @@ def write_to_exchange(exchange, obj):
 def read_from_exchange(exchange):
     return json.loads(exchange.readline())
 
+def hello(exchange):
+    write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
+
+def buy(exchange, order_id, symbol, price, size):
+    write_to_exchange(exchange, {"type": "add", "order_id": order_id, "symbol": symbol, "dir": "BUY", "price": price, "size": size})
+
+def sell(exchange, order_id, symbol, price, size)
+    write_to_exchange(exchange, {"type": "add", "order_id": order_id, "symbol": symbol, "dir": "SELL", "price": price, "size": size})
+
+def convert(exchange, order_id, symbol, size):
+    write_to_exchange(exchange, {"type": "convert", "order_id": order_id, "symbol": symbol, "dir": "BUY", "size": size})
+
+def cancel(exchange, order_id):
+    write_to_exchange(exchange, {"type": "cancel", "order_id": order_id})
+
+def get_info(exchange, buy_dict, sell_dict):
+    from_exchange = read_from_exchange(exchange)
+
+    words_arr = from_exchange.split()
+    if words_arr[0] == "BOOK":
+        security  = words_arr[1]
+        buy_str = words_arr[words_arr.index("BUY") + 1]
+        highest_buy_str = ""
+        for char in buy_str:
+            if char.isdigit():
+                highest_buy_str = highest_buy_str + char
+            else:
+                break
+        highest_buy_int = int(highest_buy_str)
+
+        sell_str = words_arr[words_arr.index("SELL") + 1]
+        lowest_sell_str = ""
+        for char in sell_str:
+            if char.isdigit():
+                lowest_sell_str = lowest_sell_str + char
+            else:
+                break
+        lowest_sell_int = int(lowest_sell_str)
+
+        buy_dict[security] = highest_buy_int
+        sell_dict[security] = lowest_sell_int
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
 def main():
     exchange = connect()
+
+    sell_dict = {}
+    buy_dict = {}
+
+    order_id = random.randint(1000, 100000)
+
     write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
     hello_from_exchange = read_from_exchange(exchange)
     # A common mistake people make is to call write_to_exchange() > 1
