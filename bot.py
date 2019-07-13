11003b45fc39d8f008952d71d427a6d8b3827c02
Bjorn Johnson <bjornjohnson@Bjorns-MacBook-Pro.local>
2019-07-13 12:40:05 -0700

    added team name

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@
 # ~~~~~==============   HOW TO RUN   ==============~~~~~
 # 1) Configure things in CONFIGURATION section
 # 2) Change permissions: chmod +x bot.py
-# 3) Run in loop: while true; do ./bot.py; sleep 1; done
+# 3) Run in loop: init_bot.py && while true; do ./bot.py; sleep 1; done
 
 from __future__ import print_function
 
@@ -41,6 +41,34 @@ def write_to_exchange(exchange, obj):
 def read_from_exchange(exchange):
     return json.loads(exchange.readline())
 
+# ~~~~~============== PARAMETERS ==============~~~~~
+increment = 1
+decrement = 1
+
+# ~~~~~============== BOT FUNCTIONS ==============~~~~~
+def hello(exchange):
+    write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
+
+def buy(exchange, order_id, symbol, price, size):
+    write_to_exchange(exchange, {"type": "add", "order_id": order_id, "symbol": symbol, "dir": "BUY", "price": price, "size": size})
+
+def sell(exchange, order_id, symbol, price, size)
+    write_to_exchange(exchange, {"type": "add", "order_id": order_id, "symbol": symbol, "dir": "SELL", "price": price, "size": size})
+
+def convert(exchange, order_id, symbol, size):
+    write_to_exchange(exchange, {"type": "convert", "order_id": order_id, "symbol": symbol, "dir": "BUY", "size": size})
+
+def cancel(exchange, order_id):
+    write_to_exchange(exchange, {"type": "cancel", "order_id": order_id})
+
+def penny_buy(symbol, high, quantity)
+    buy(symbol, high+increment, quantity)
+
+def penny_sell(symbol, low, quantity)
+    buy(symbol, low-decrement, quantity)
+
+def get_fair_price(symbol, high, low)
+    return (high+low)/2
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
@@ -54,6 +82,15 @@ def main():
     # exponential explosion in pending messages. Please, don't do that!
     print("The exchange replied:", hello_from_exchange, file=sys.stderr)
 
+    buy(BOND, 999, 1)
+    sell(BOND, 1001, 1)
+
+    # for symbol in sym_list :
+    #     fair = get_fair_price(symbol, high, low)
+    #     if fair>prev_fair :
+    #         penny_buy(symbol, )
+    #     elif
+
+
 if __name__ == "__main__":
     main()
-
